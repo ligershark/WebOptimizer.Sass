6b6e44f927377b52d3e68f395c51935a8b3f0647
ZeroXitreo <3112566+ZeroXitreo@users.noreply.github.com>
2023-12-28 14:58:07 +0100

    Add ManifestFileManager

---
+++ src/ManifestFileManager.cs
@@ -0,0 +1,60 @@
+﻿using DartSassHost;
+using JavaScriptEngineSwitcher.Core.Resources;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.IO;
+using System.Text;
+
+namespace WebOptimizer.Sass
+{
+	public class ManifestFileManager : IFileManager
+	{
+		private readonly IFileProvider _fileProvider;
+
+		public ManifestFileManager(IFileProvider fileProvider)
+		{
+			_fileProvider = fileProvider;
+		}
+
+		public bool SupportsVirtualPaths => false;
+
+		public bool FileExists(string path)
+		{
+			IFileInfo file = _fileProvider.GetFileInfo(path);
+			return file.Exists;
+		}
+
+		public string GetCurrentDirectory()
+		{
+			return "";
+		}
+
+		public bool IsAppRelativeVirtualPath(string path)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string ReadFile(string path)
+		{
+			if (path == null)
+			{
+				throw new ArgumentNullException(
+					nameof(path),
+					string.Format(Strings.Common_ArgumentIsNull, nameof(path))
+				);
+			}
+
+			path = path.TrimStart('/');
+			path = path.TrimStart('\\');
+			IFileInfo file = _fileProvider.GetFileInfo(path);
+			using Stream stream = file.CreateReadStream();
+			using StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+			return reader.ReadToEnd();
+		}
+
+		public string ToAbsoluteVirtualPath(string path)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
