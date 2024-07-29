cca9aa0fc4228460854024c1d875fc22c6ebdbd8
Alexander Reinert <alex@areinert.de>
2024-07-29 22:55:25 +0200

    * Reworked Processor to not use physical file access, instead only use IFileProvider * Extended tests of Processor * upgrade all projects to .net 8 * upgrade all nuget dependencies

--- test/CompilerTest.cs
+++ test/CompilerTest.cs
@@ -4,15 +4,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.FileProviders.Physical;
 using Microsoft.Extensions.Primitives;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using WebOptimizer.Sass.Test.Mocks;
+using WebOptimizer.Utils;
 using Xunit;
 
 namespace WebOptimizer.Sass.Test
@@ -30,8 +29,15 @@ namespace WebOptimizer.Sass.Test
         }
 
 
-        [Fact]
-        public async Task Compile_Success()
+        [Theory]
+        [InlineData("$bg: blue; div {background: $bg}", "div {\n  background: blue;\n}")]
+        [InlineData("@import 'test';\ndiv {background: $bg}", "div {\n  background: red;\n}")]
+        [InlineData("@import '../test';\ndiv {background: $bg}", "div {\n  background: green;\n}")]
+        [InlineData("@import '/test';\ndiv {background: $bg}", "div {\n  background: green;\n}")]
+        [InlineData("@import 'sub/test';\ndiv {background: $bg}", "div {\n  background: white;\n}")]
+        [InlineData("@import '/css/sub/test';\ndiv {background: $bg}", "div {\n  background: white;\n}")]
+        [InlineData("@import 'variables';\ndiv {background: $bg}", "div {\n  background: black;\n}")]
+        public async Task Compile_Success(string scssContent, string compiled)
         {
             var context = new Mock<IAssetContext>().SetupAllProperties();
             var asset = new Mock<IAsset>().SetupAllProperties();
@@ -39,8 +45,26 @@ namespace WebOptimizer.Sass.Test
             var env = new Mock<IWebHostEnvironment>();
             var fileProvider = new Mock<IFileProvider>();
 
+            fileProvider.Setup(f => f.GetFileInfo(It.IsAny<string>()))
+                .Returns((string path) => new NotFoundFileInfo(UrlPathUtils.GetFileName(path)));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/file.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("file.scss", new DateTime(2017, 1, 1), scssContent.AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test.scss", new DateTime(2017, 1, 1), "$bg: red;".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/test.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test.scss", new DateTime(2017, 1, 1), "$bg: green;".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/sub/test.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test.scss", new DateTime(2017, 1, 1), "$bg: white;".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/_variables.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("_variables.scss", new DateTime(2017, 1, 1), "$bg: black;".AsByteArray()));
+
             context.Object.Content = new Dictionary<string, byte[]> {
-                { "/file.css", "$bg: blue; div {background: $bg}".AsByteArray() },
+                { "css/file.scss", scssContent.AsByteArray() },
             };
 
             context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IWebHostEnvironment)))
@@ -49,20 +73,15 @@ namespace WebOptimizer.Sass.Test
             context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IJsEngineSwitcher)))
                    .Returns(JsEngineSwitcher.Current);
 
-            var inputFile = new PhysicalFileInfo(new FileInfo("site.css"));
-
             context.SetupGet(s => s.Asset)
                           .Returns(asset.Object);
 
             env.SetupGet(e => e.WebRootFileProvider)
                  .Returns(fileProvider.Object);
 
-            fileProvider.Setup(f => f.GetFileInfo(It.IsAny<string>()))
-                   .Returns(inputFile);
-
             await processor.ExecuteAsync(context.Object);
             var result = context.Object.Content.First().Value;
-            Assert.Equal("div {\n  background: blue;\n}", result.AsString());
+            Assert.Equal(compiled, result.AsString());
         }
 
         [Fact]
@@ -82,65 +101,34 @@ namespace WebOptimizer.Sass.Test
 
             // Setup files
             var fileProvider = new Mock<IFileProvider>();
+
+            fileProvider.Setup(f => f.GetFileInfo(It.IsAny<string>()))
+                .Returns((string path) => new NotFoundFileInfo(UrlPathUtils.GetFileName(path)));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test1.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test1.scss", new DateTime(2017, 1, 1), "@import 'test2'; \r\n @import \"test3\";".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test2.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test2.scss", new DateTime(2017, 1, 1), "@import '../test4';".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test3.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test3.scss", new DateTime(2017, 1, 1), "@import 'http://localhost/'; \r\n @use 'test5';".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test4.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test4.scss", new DateTime(2017, 1, 1), "@import 'http://localhost/';".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test5.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test5.scss", new DateTime(2017, 1, 1), "@use '../test6';".AsByteArray()));
+
+            fileProvider.Setup(f => f.GetFileInfo(It.Is<string>(value => value.Equals("/css/test6.scss", StringComparison.InvariantCultureIgnoreCase))))
+                .Returns(new MockFileInfo("test6.scss", new DateTime(2017, 1, 1), "@use 'http://localhost/';".AsByteArray()));
+
             env.Setup(e => e.WebRootFileProvider)
                 .Returns(fileProvider.Object);
-            var test1 = new Mock<IFileInfo>();
-            test1.Setup(f => f.Exists).Returns(true);
-            test1.Setup(f => f.PhysicalPath).Returns("css/test1.scss");
-            test1.SetupSequence(f => f.CreateReadStream())
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import 'test2'; \r\n @import \"test3\";")))
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import 'test2'; \r\n @import \"test3\";")));
-
-            var test2 = new Mock<IFileInfo>();
-            test2.Setup(f => f.Exists).Returns(true);
-            test2.Setup(f => f.PhysicalPath).Returns("css/test2.scss");
-            test2.SetupSequence(f => f.CreateReadStream())
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import '../test4';")))
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import '../test4';")));
-
-            var test3 = new Mock<IFileInfo>();
-            test3.Setup(f => f.Exists).Returns(true);
-            test3.Setup(f => f.PhysicalPath).Returns("css/test3.scss");
-            test3.SetupSequence(f => f.CreateReadStream())
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import 'http://localhost/'; \r\n @use 'test5';")))
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import 'http://localhost/'; \r\n @use 'test5';")));
-
-            var test4 = new Mock<IFileInfo>();
-            test4.Setup(f => f.Exists).Returns(true);
-            test4.Setup(f => f.PhysicalPath).Returns("test4.scss");
-            test4.SetupSequence(f => f.CreateReadStream())
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import 'http://localhost/';")))
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@import 'http://localhost/';")));
-
-            var test5 = new Mock<IFileInfo>();
-            test5.Setup(f => f.Exists).Returns(true);
-            test5.Setup(f => f.PhysicalPath).Returns("css/test5.scss");
-            test5.SetupSequence(f => f.CreateReadStream())
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@use '../test6';")))
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@use '../test6';")));
-
-            var test6 = new Mock<IFileInfo>();
-            test6.Setup(f => f.Exists).Returns(true);
-            test6.Setup(f => f.PhysicalPath).Returns("test6.scss");
-            test6.SetupSequence(f => f.CreateReadStream())
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@use 'http://localhost/';")))
-                .Returns(new MemoryStream(Encoding.Default.GetBytes("@use 'http://localhost/';")));
 
             var changeToken = new Mock<IChangeToken>();
             fileProvider.Setup(p => p.Watch(It.IsAny<string>()))
                 .Returns(changeToken.Object);
-            fileProvider.Setup(p => p.GetFileInfo("css/test1.scss"))
-                .Returns(test1.Object);
-            fileProvider.Setup(p => p.GetFileInfo("css/test2.scss"))
-                .Returns(test2.Object);
-            fileProvider.Setup(p => p.GetFileInfo("css/test3.scss"))
-                .Returns(test3.Object);
-            fileProvider.Setup(p => p.GetFileInfo("test4.scss"))
-                .Returns(test4.Object);
-            fileProvider.Setup(p => p.GetFileInfo("css/test5.scss"))
-                .Returns(test5.Object);
-            fileProvider.Setup(p => p.GetFileInfo("test6.scss"))
-                .Returns(test6.Object);
 
             // Setup Cache
             var cache = new Mock<IMemoryCache>();
---
+++ test/Mocks/MockFileInfo.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebOptimizer.Sass.Test.Mocks
+{
+    internal class MockFileInfo : IFileInfo
+    {
+        private readonly byte[] _data;
+
+        public MockFileInfo(string name, DateTimeOffset lastModified, byte[] data)
+        {
+            _data = data;
+            Name = name;
+            LastModified = lastModified;
+        }
+
+        public Stream CreateReadStream()
+        {
+            return new MemoryStream(_data, false);
+        }
+
+        public bool Exists => true;
+        public bool IsDirectory => false;
+        public DateTimeOffset LastModified { get; }
+        public long Length => _data.Length;
+        public string Name { get; }
+        public string PhysicalPath => null;
+    }
+}
